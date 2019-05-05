4fc9cce5413f459eece7ab2306ba31d06fbcb078
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2019-05-04 19:16:37 -0700

    Enable LD_LIBRARY_PATH for Firebase

--- FirebaseFunction/functions/index.js
+++ FirebaseFunction/functions/index.js
@@ -1,4 +1,6 @@
 const functions = require("firebase-functions");
 const service = require("./service");
 
+service.needsLibraryPath = true;
+
 exports.compile = functions.https.onRequest(service.app);
