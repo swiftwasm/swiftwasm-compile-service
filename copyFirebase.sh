f82b1d90fb2d3caa9b1e19ecdd052d0850430a5e
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2019-05-04 19:52:11 -0700

    Copy extra_objs

--- copyFirebase.sh
+++ copyFirebase.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
-rm -r FirebaseFunction/functions/compiler FirebaseFunction/functions/service.js || true
+rm -r FirebaseFunction/functions/compiler FirebaseFunction/functions/service.js \
+	FirebaseFunction/functions/extra_objs || true
 cp service.js FirebaseFunction/functions/
 cp -a compiler FirebaseFunction/functions/compiler
+cp -a extra_objs FirebaseFunction/functions/extra_objs
