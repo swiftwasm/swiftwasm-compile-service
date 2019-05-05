47e0b930bf56b3f44987949f75279648376d6743
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2019-05-04 21:35:05 -0700

    Generate glibc.modulemap for Firebase

--- copyFirebase.sh
+++ copyFirebase.sh
@@ -5,3 +5,5 @@ rm -r FirebaseFunction/functions/compiler FirebaseFunction/functions/service.js 
 cp service.js FirebaseFunction/functions/
 cp -a compiler FirebaseFunction/functions/compiler
 cp -a extra_objs FirebaseFunction/functions/extra_objs
+bash generateModulemap.sh "/srv/functions/compiler/wasi-sdk/opt/wasi-sdk/share/sysroot" \
+	>FirebaseFunction/functions/compiler/opt/swiftwasm-sdk/lib/swift/wasm/wasm32/glibc.modulemap
